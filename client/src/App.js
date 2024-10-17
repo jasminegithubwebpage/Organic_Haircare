715f4c19e0930ccb5b28bde2001402202440d923
K.M.Prabhanjan <prabhanjan1506@gmail.com>
2024-10-17 22:42:42 +0530

    about

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import DashProduct from "./pages/DashProduct";
 import PaymentForm from "./components/PaymentForm";
 import AddProduct from "./pages/AddProduct";
 import Login from "./pages/Login";
+import About from "./pages/About";
 // import UserLogin from "./pages/UserLogin";
 // import Dashboard from "./pages/Dashboard";
 import SuperAdminDashboard from "./pages/SuperAdminDashBoard";
@@ -26,6 +27,14 @@ function App() {
           element={
             <Layout>
               <LandingPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <About />
             </Layout>
           }
         />
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,132 @@
-import AboutContent from "../components/AboutContent";
-import AboutDetails from "../json/AboutDetails";
+function About() {
+  return (
+    <div className="whole">
+    <div className="about">
+      <h1>About Us</h1>
+      <div className="about_container">
+        <div>
+          <h4>Organic Supply</h4>
+          <p>
+            Our shampoo provides effective and fast therapy for hair loss and
+            its treatment ensures excellent therapeutic results. Recommended as
+            a treatment for hair loss, hair regeneration, dandruff, alopecia,
+            baldness, and fragile and dry hair.
+          </p>
+        </div>
+        <div>
+          <h4>Facts</h4>
+          <p>
+            Our shampoo is the only natural product with proven results with
+            millions of satisfied men and women.
+          </p>
+          <p>
+            It is proven to be effective against baldness, dandruff, hair loss,
+            excessive sebum in the scalp, greasy hair, and alopecia.
+          </p>
+        </div>
 
-function About(){
-    return(
-        
-    );
+        <div>
+          <h4>Overview</h4>
+          <p>
+            For thousands of years, men and women have been searching for the
+            solution to hair loss which can develop over time into baldness.
+            This search is associated with dissatisfaction with personal
+            welfare, which can cause psychological problems. The hair needs
+            care, especially for those who are suffering problems with baldness,
+            dandruff, hair loss, excessive sebum, greasy, alopecia, and weak
+            hair. So we came up with a better solution for solving it.
+          </p>
+        </div>
+
+        <div>
+          <h4>Scientific Explanation</h4>
+          <p>
+            Our products was tested on hundreds of patients, men, and women of
+            all ages, with positive effects. This evidence made us study plants
+            with pharmacological properties that can stimulate human hair growth
+            or delay it's falling out and reduce baldness, dandruff, hair loss,
+            excessive sebum in the scalp, greasy hair, and alopecia.
+          </p>
+          <p>
+            The vessel dilatation effect viewed by Scientists was greater than
+            expected, considering the pilocarpina concentration found in it.
+            This activity proved an increase in nutrients diffusion to the
+            scalp, improving nourishment of the follicle, and resulting in hair
+            recuperation. It is proven to be effective against baldness,
+            dandruff, hair loss, excessive sebum in the scalp, greasy hair, and
+            alopecia.
+          </p>
+        </div>
+
+        <div>
+          <h4>Treatment Cycle</h4>
+          <p>
+            1st and 2nd month: Restores the hair follicle degenerated,
+            recovering the root of the hair, eliminating dandruff, and excessive
+            sebun in the scalp and resulting consequently in the end of grease
+            excess.{" "}
+          </p>
+          <p>2nd and 3rd month: The hair loss ends.</p>
+          <p>
+            3rd, 4th, and 5th month: The growth of the hair starts again. The
+            product must be applied to the scalp twice a day, gently massaging
+            into the scalp for 30 seconds. You do not need to wash or rinse. The
+            benefits arise in different styles from organism to organism. At the
+            beginning of the treatment, it's normal that the weak hair falls, to
+            then grow other new, strong, and beautiful hair.
+          </p>
+        </div>
+        <div>
+          <h4>Capillary Problems</h4>
+          <p>
+            BALDNESS: The most common form of hair loss, no doubt it is called
+            Alopecia or male .baldness. These symptoms can be associated with a
+            high level of male hormones in the .blood. This kind of baldness is
+            caused by the increase of alpha 5 reeducates enzymes in the scalp.
+          </p>
+          <p>
+            DANDRUFF: Scale of the scalp. It is caused by fungus proliferation.
+          </p>
+          <p>
+            HAIR LOSS: Caused by the weakness of the capillary root, which
+            causes hair to fall out.
+          </p>
+          <p>
+            DRY AND FRAGILE HAIR: The sun, the wind, and pollution are all
+            factors that cause the common problems of dry and fragile hair.
+          </p>
+          <p>
+            GREASY HAIR: Hyper secretion by sebaceous glands causes excessive
+            greasiness in the scalp{" "}
+          </p>
+          <p>
+            HAIR BRIGHTNESS, VOLUME, AND SLOW GROWTH: This problem is caused due
+            to the atrophy of the hair follicle.{" "}
+          </p>
+        </div>
+        <div>
+          <h4>Direction of Use</h4>
+          <p>
+            This product must be applied to the scalp twice daily, gently
+            massaging it into the scalp for 30 seconds. You do not need to wash
+            or rinse the hair. Patients using ervamatin are advised to use an
+            Organic shampoo when washing their hair. It is recommended to wash
+            your hair daily if you have oily hair, but you can wash it 4 times
+            if your hair is normal. After washing it, use Ervamatin Lotion and
+            don't rinse.{" "}
+          </p>
+        </div>
+        <div>
+          <h4>Tips</h4>
+          <p>
+            Use a neutral or natural shampoo. To keep the beauty of hair is
+            necessary to have a healthy alimentation.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 }
+
+export default About;
